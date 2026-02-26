85f2ea6ae64dd59585114fc9c8cd440ae446dd20
chinlinlee <a5566qq2581@gmail.com>
2026-02-26 22:16:58 +0800

    feat: add `DicomReadableBufferStream` export

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import { DicomMessageAsync } from "./DicomMessageAsync.js";
 import { readFrame } from "./readFrame.js";
 import { TagAsync } from "./TagAsync.js";
 import { StreamValueRepresentation } from "./StreamValueRepresentation.js";
+import { DicomReadableBufferStream } from "./DicomReadableBufferStream.js";
 
-export { DicomMessageAsync, readFrame, TagAsync, StreamValueRepresentation };
+export { DicomMessageAsync, readFrame, TagAsync, StreamValueRepresentation, DicomReadableBufferStream };
